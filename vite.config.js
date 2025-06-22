d1de9b17a11dd87a2bd5b10203896bbec4214a4e
SimarjeetSingh0 <166621815+SimarjeetSingh0@users.noreply.github.com>
2025-06-22 10:30:00 +0530

    Add files via upload

---
+++ vite.config.js
@@ -0,0 +1,15 @@
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [react()],
+  // Add this to ensure environment variables are loaded
+  envDir: '.',
+  define: {
+    'process.env': {
+     
+
+    }
+  }
+})
